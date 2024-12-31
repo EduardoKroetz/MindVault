2c891de3a8a96fd3655fc1a5b9c5434a5b4132ae
EduardoKroetz <eduardokroetz0@gmail.com>
2024-12-30 21:00:07 -0300

    feat: add header styles

--- src/MindVault.Web/client-app/src/app/components/header/header.tsx
+++ src/MindVault.Web/client-app/src/app/components/header/header.tsx
@@ -1,17 +1,20 @@
+import Link from "next/link"
 import styles from "./header.module.css" 
 
 export default function Header()
 {
   return (
     <div className={styles.header}>
-      <div>
-        <h1>MindVault</h1>
-        <div>
-          <a href="/notes">Anotações</a>
-          <a href="/categories">Categorias</a>
+      <nav className={styles.nav}>
+        <a className={styles.title} href="/">
+          <h1>MindVault</h1>
+        </a>
+        <div className={styles.links}>
+          <Link href={"/notes"}>Anotações</Link>
+          <Link href={"/categories"}>Categorias</Link>
         </div>
-      </div>
-      <i></i>
+      </nav>
+      <i  className={"bi bi-person-circle " + styles.user}></i>    
     </div>
   )
 }
